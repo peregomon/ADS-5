2297bf96d155b832d5b89de3224c72e28c0a80dc
peregomon <160238970+peregomon@users.noreply.github.com>
2024-04-21 13:32:41 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,41 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    int top;
+    T* stack;
+
+ public:
+    TStack() : top(-1), stack(new T(size)) {}
+    char Top(void) {
+        if (IsEmpty()) throw std::string("Stack is EMPTY!");
+        else
+            return stack[top];
+    }
+    void push(T value) {
+        if (top == size - 1) {
+            throw std::string("Stack is FULL!");
+        } else {
+            top++;
+            stack[top] = value;
+        }
+    }
+    const T& pop() {
+        if (IsEmpty()) {
+            throw std::string("Stack is EMPTY!");
+        }
+        top--;
+        return stack[top + 1];
+    }
+    bool isEmpty() {
+        if (top == -1) return true;
+        else
+            return false;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
