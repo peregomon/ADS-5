7ca010af85597173b4e24944622ce73f756196b1
peregomon <160238970+peregomon@users.noreply.github.com>
2024-04-21 13:36:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,41 +1,58 @@
-// Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
-#include <string>
+#include "tstack.h"
 
-template<typename T, int size>
-class TStack {
- private:
-    int top;
-    T* stack;
-
- public:
-    TStack() : top(-1), stack(new T(size)) {}
-    char Top(void) {
-        if (IsEmpty()) throw std::string("Stack is EMPTY!");
-        else
-            return stack[top];
-    }
-    void Push(T value) {
-        if (top == size - 1) {
-            throw std::string("Stack is FULL!");
-        } else {
-            top++;
-            stack[top] = value;
+std::string infx2pstfx(std::string inf) {
+    TStack<char, 100> stack1;
+    std::map<char, int>  priority  = {{'(', 0}, { ')', 1}, { '+', 2},
+                                      { '-', 2}, { '*', 3}, { '/', 3} };
+    std::string res_str = "";
+    for (int i = 0; i < inf.length(); i++) {
+        if ('0' <= inf[i] && inf[i] <= '9') {
+            res_str += inf[i];
+            res_str += " ";
+        } else if (inf[i] == '(') {
+            stack1.push(inf[i]);
+        } else if (inf[i] == ')') {
+            while (!stack1.isEmpty() && stack1.top() != '(') {
+                res_str += stack1.Pop();
+                res_str += " ";
+            }
+            stack1.pop();
+        } else if (inf[i] == '+' || inf[i] == '-'
+                   || inf[i] == '*' || inf[i] == '/') {
+            while ( !stack1.isEmpty()
+                   && priority[inf[i]] <= priority[stack1.top()]) {
+                res_str += stack1.pop();
+                res_str += " ";
+            }
+            stack1.push(inf[i]);
         }
     }
-    const T& Pop() {
-        if (IsEmpty()) {
-            throw std::string("Stack is EMPTY!");
-        }
-        top--;
-        return stack[top + 1];
+    while (!stack1.isEmpty()) {
+        res_str += stack1.pop();
+        res_str += " ";
     }
-    bool IsEmpty() {
-        if (top == -1) return true;
-        else
-            return false;
-    }
-};
+    res_str.erase(res_str.length() - 1);
+    return res_str;
+}
 
-#endif  // INCLUDE_TSTACK_H_
+int eval(std::string pref) {
+    TStack<int, 100> stack2;
+    for (int i = 0; i < pref.length(); i++) {
+        if ('0' <= pref[i] && pref[i] <= '9') {
+            stack2.push(pref[i] - '0');
+        } else if (pref[i] == '+') {
+            stack2.push(stack2.pop() + stack2.pop());
+        } else if (pref[i] == '-') {
+            int num2 = stack2.pop();
+            int num1 = stack2.pop();
+            stack2.push(num1 - num2);
+        } else if (pref[i] == '*') {
+            stack2.push(stack2.pop() * stack2.pop());
+        } else if (pref[i] == '/') {
+            int num2 = stack2.pop();
+            int num1 = stack2.pop();
+            stack2.push(num1 / num2);
+        }
+    }
+    return stack2.top();
+}
